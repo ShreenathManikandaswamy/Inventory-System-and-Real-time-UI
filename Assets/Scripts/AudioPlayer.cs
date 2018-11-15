75960d496bd1299b28e8bd4654594d4412e8be90
Shreenath Manikandaswamy <shreenath585@gmail.com>
2018-11-15 12:50:22 +0530

    Repository system and Real time UI
    This is a project done for an interview and it took me 4 hours to complete it

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    public AudioSource player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        player = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio()
+    {
+        player.Play();
+    }
+}
